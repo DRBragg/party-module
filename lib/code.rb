86849d43bbdde4a3d1ee557c94015dab0121e4cc
Drew Bragg <DRBragg@gmail.com>
2017-07-26 18:32:39 -0400

    completed work

--- lib/code.rb
+++ lib/code.rb
@@ -1,3 +1,36 @@
 module PartyGoer
-  #YOUR CODE GOES HERE
+
+  def initialize
+    @drinkCount = 0
+  end
+
+  def drink
+    if @drinkCount < 3
+      @drinkCount += 1
+      return true
+    else
+      return false
+    end
+  end
+
+  def sing
+    system("say Party party party!")
+    return "I like to party!"
+  end
+
+  def cause_havoc
+      raise PersonalizedHavocError
+  end
+
+  def invited?
+    true
+  end
+end
+
+class PersonalizedHavocError < StandardError
+
+  def initialize(msg="Chaos!")
+    super
+  end
+
 end
